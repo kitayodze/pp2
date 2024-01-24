92b09debe9bd6d0872ae474bd871bed575e9be2f
unknown <sapuanovasset2006@gmail.com>
2024-01-24 23:58:02 +0600

    data_types

---
+++ data_types.py
@@ -0,0 +1,27 @@
+x = 5
+print(type(x))
+
+int # Ex1
+x = "Hello, World"
+print(type(x))
+
+str #Ex2
+x = 20.5
+print(type(x))
+
+float #Ex3
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+list #Ex4
+x = ("apple", "banana", "cherry")
+
+tuple #Ex5
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+dict #Ex6
+x = True
+print(type(x))
+
+bool#Ex7
