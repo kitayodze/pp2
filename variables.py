b639490ee1d456c9c4776d72c7bd8ab306d7a607
unknown <sapuanovasset2006@gmail.com>
2024-01-24 23:41:49 +0600

    variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+carname = "Volvo" #Ex1
+x = 50 #Ex2
+x = 5
+y = 10
+print (x + y) #Ex3
+x = 5
+y = 10
+z= x + y
+print(z) #Ex 4
+x,y,z = "Orange", "Banana", "Cherry" #Ex5
+x=y=z= "Orange" #Ex6
+def myfunc():
+    global x
+    x = "fantastic" #Ex7
