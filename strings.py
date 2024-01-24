7b5b2576f0390f87d1b5a08e183409ebf2f35363
unknown <sapuanovasset2006@gmail.com>
2024-01-25 00:27:08 +0600

    strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+x = "Hello, World"
+print(len(x)) #Ex1
+txt = "Hello. World"
+x = txt[0] #Ex2
+txt = "Hello, World"
+x = txt[2:5] #Ex3
+txt = " Hello World"
+x = txt.strip() #Ex4
+txt = "Hello, World"
+txt = txt.upper() #Ex5
+txt = "Hello, World"
+txt = txt.lower() #Ex6
+txt = "Hello, World"
+txt = txt.replace( "H", "J") #Ex7
+age = 36
+txt = " My name is John, and I am {}"
+print(txt.format(age)) #Ex8
